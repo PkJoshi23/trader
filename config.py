e66404748ade55be031390ca611353c0ed8e14e0
PkJoshi23 <126094816+PkJoshi23@users.noreply.github.com>
2025-06-16 22:10:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ ALPACA_API_KEY = os.getenv('APCA_API_KEY_ID') or os.getenv('ALPACA_API_KEY')
 ALPACA_SECRET_KEY = os.getenv('APCA_API_SECRET_KEY') or os.getenv('ALPACA_SECRET_KEY')
 ALPACA_BASE_URL = os.getenv('APCA_API_BASE_URL', 'https://paper-api.alpaca.markets')
 # Trading parameters
-MAX_RISK_PER_TRADE = 0.01  # 1% of total capital
+MAX_RISK_PER_TRADE = 0.20  # 1% of total capital
 STOP_LOSS_ATR_MULTIPLIER = 1.5
 TAKE_PROFIT_ATR_MULTIPLIER = 2.5
 MAX_CONCURRENT_POSITIONS = 3
-DAILY_MAX_LOSS_CAP = 0.05  # 5% of account balance
+DAILY_MAX_LOSS_CAP = 0.20  # 5% of account balance
 
 # Timeframes
 TIMEFRAMES = ['1Min', '5Min']
@@ -21,8 +21,4 @@ TIMEFRAMES = ['1Min', '5Min']
 MAX_STOCK_PRICE = 50.0  # Only trade stocks below $5
 MIN_VOLUME_SPIKE = 0.8  # 2x average volume spike
 
-# Market session focus
-PRE_MARKET_START = '04:00'
-MARKET_OPEN = '09:30'
-MARKET_CLOSE = '16:00'
-EOD_REVERSAL_START = '15:00' 
+
